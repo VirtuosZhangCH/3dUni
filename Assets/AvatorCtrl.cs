dbce621d681339e0125967d517bf9ab43a1d1239
jeff <unnamedzc@163.com>
2015-06-18 18:10:29 +0800

    222

---
+++ Assets/AvatorCtrl.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+
+public class AvatorCtrl : MonoBehaviour {
+
+    protected Animator animator;
+    public float DirectionDampTime = .25f;
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(animator)
+        {
+            AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+
+            if(stateInfo.nameHash == Animator.StringToHash("Base Layer.Idle"))
+            {
+                if (Input.GetKeyUp("1"))
+                    animator.SetBool("Attack",true);
+                else
+                    animator.SetBool("Attack", false);
+            }
+
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
+
+            animator.SetFloat("Speed", h * h + v * v);
+            animator.SetFloat("Direction", h, DirectionDampTime, Time.deltaTime);
+        }
+	}
+}
